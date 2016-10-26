39c80f89223c9e123265deaa275fdeb0f97d4a09
Alberto Marchetti <cmaster11@gmail.com>
2016-10-26 09:23:47 +0300

    Added support for babelPluginTransformFunctionBind

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-'use strict'
+'use strict';
 
-const babelPluginTransformES2015ModulesCommonjs = require('babel-plugin-transform-es2015-modules-commonjs')
-const babelPluginTransformObjectRestSpread = require('babel-plugin-transform-object-rest-spread')
-const babelPluginSyntaxTrailingFunctionCommas = require('babel-plugin-syntax-trailing-function-commas')
+const babelPluginTransformES2015ModulesCommonjs = require('babel-plugin-transform-es2015-modules-commonjs');
+const babelPluginTransformObjectRestSpread = require('babel-plugin-transform-object-rest-spread');
+const babelPluginSyntaxTrailingFunctionCommas = require('babel-plugin-syntax-trailing-function-commas');
+const babelPluginTransformFunctionBind = require('babel-plugin-transform-function-bind');
 
 module.exports = {
   plugins: [
     babelPluginTransformES2015ModulesCommonjs,
     babelPluginTransformObjectRestSpread,
     babelPluginSyntaxTrailingFunctionCommas,
+    babelPluginTransformFunctionBind,
   ],
-}
+};
